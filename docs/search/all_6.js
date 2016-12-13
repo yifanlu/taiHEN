cf0c950daee2ce01359fbdc43c6999471441f5ac
Yifan Lu <me@yifanlu.com>
2016-12-12 19:17:54 -0800

    Updated docs

--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -4,6 +4,8 @@ var searchData=
   ['helper',['HELPER',['../group__taihen.html#ga5cde8e1b2cda53e38dbb028451c84074',1,'taihen.h']]],
   ['homebrew_20enabler',['Homebrew Enabler',['../group__hen.html',1,'']]],
   ['hen_5fadd_5fpatches',['hen_add_patches',['../group__hen.html#gac8d35fe02a43e0168de9e94aa703a8d5',1,'hen_add_patches(void):&#160;hen.c'],['../group__hen.html#gac8d35fe02a43e0168de9e94aa703a8d5',1,'hen_add_patches(void):&#160;hen.c']]],
+  ['hen_5ffree_5fconfig',['hen_free_config',['../group__hen.html#gaf5060fe23460603d2caba2f9632547c0',1,'hen_free_config(void):&#160;hen.c'],['../group__hen.html#gaf5060fe23460603d2caba2f9632547c0',1,'hen_free_config(void):&#160;hen.c']]],
+  ['hen_5fload_5fconfig',['hen_load_config',['../group__hen.html#ga701e2dbde37654ae29a88cde3ff41b38',1,'hen_load_config(void):&#160;hen.c'],['../group__hen.html#ga701e2dbde37654ae29a88cde3ff41b38',1,'hen_load_config(void):&#160;hen.c']]],
   ['hen_5fload_5fplugin',['hen_load_plugin',['../group__hen.html#ga3915ab72104a06224955796f1211be49',1,'hen_load_plugin(const char *path, void *param):&#160;hen.c'],['../group__hen.html#ga3915ab72104a06224955796f1211be49',1,'hen_load_plugin(const char *module, void *param):&#160;hen.c']]],
   ['hen_5fremove_5fpatches',['hen_remove_patches',['../group__hen.html#ga73fabfcf7176236aaa5dfcd724eee3dd',1,'hen_remove_patches(void):&#160;hen.c'],['../group__hen.html#ga73fabfcf7176236aaa5dfcd724eee3dd',1,'hen_remove_patches(void):&#160;hen.c']]],
   ['hooks_20interface',['Hooks Interface',['../group__hook.html',1,'']]],
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['hen_5fadd_5fpatches',['hen_add_patches',['../group__hen.html#gac8d35fe02a43e0168de9e94aa703a8d5',1,'hen_add_patches(void):&#160;hen.c'],['../group__hen.html#gac8d35fe02a43e0168de9e94aa703a8d5',1,'hen_add_patches(void):&#160;hen.c']]],
+  ['hen_5ffree_5fconfig',['hen_free_config',['../group__hen.html#gaf5060fe23460603d2caba2f9632547c0',1,'hen_free_config(void):&#160;hen.c'],['../group__hen.html#gaf5060fe23460603d2caba2f9632547c0',1,'hen_free_config(void):&#160;hen.c']]],
+  ['hen_5fload_5fconfig',['hen_load_config',['../group__hen.html#ga701e2dbde37654ae29a88cde3ff41b38',1,'hen_load_config(void):&#160;hen.c'],['../group__hen.html#ga701e2dbde37654ae29a88cde3ff41b38',1,'hen_load_config(void):&#160;hen.c']]],
   ['hen_5fload_5fplugin',['hen_load_plugin',['../group__hen.html#ga3915ab72104a06224955796f1211be49',1,'hen_load_plugin(const char *path, void *param):&#160;hen.c'],['../group__hen.html#ga3915ab72104a06224955796f1211be49',1,'hen_load_plugin(const char *module, void *param):&#160;hen.c']]],
   ['hen_5fremove_5fpatches',['hen_remove_patches',['../group__hen.html#ga73fabfcf7176236aaa5dfcd724eee3dd',1,'hen_remove_patches(void):&#160;hen.c'],['../group__hen.html#ga73fabfcf7176236aaa5dfcd724eee3dd',1,'hen_remove_patches(void):&#160;hen.c']]]
 ];
